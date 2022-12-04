0293fbb6b007ebd6e239ef751b1b7bf7489809fd
Bertin-Clement-Saft <112069438+Bertin-Clement-Saft@users.noreply.github.com>
2022-12-04 17:21:26 +0100

    CB: Add Day 4 (no one-liner)0

---
+++ Day4/code_day4.py
@@ -0,0 +1,31 @@
+# Set file to use as input
+filename: str = 'input.txt'
+
+# Get file content
+with open(filename) as f:
+    lines_list: 'list(str)' = [line.rstrip() for line in f]
+    
+    # Initialize results sum
+    result_p1: int = 0
+    result_p2: int = 0
+    
+    # Iterate through lines for part 1
+    for line in lines_list:
+        
+        # Get each elf section assignment
+        elf1: 'list(int)' # Size 2
+        elf2: 'list(int)' # Size 2
+        elf1, elf2 = list(map(lambda x: list(map(int, x.split('-'))), line.split(",")))
+        
+        # Detect if a section is encapsulated inside the other
+        if ((elf1[0] >= elf2[0] and elf1[1] <= elf2[1]) or
+            (elf2[0] >= elf1[0] and elf2[1] <= elf1[1])):
+            result_p1 += 1
+            
+        # Detect overlapping sections
+        if not((elf1[1] < elf2[0]) or (elf2[1] < elf1[0])):
+            result_p2 += 1
+
+# Pint results
+print(result_p1)
+print(result_p2)
